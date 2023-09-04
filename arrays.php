f8dfdb50174fb23c48124b5d891958131c70185a
Prince Asiedu <princekofasiedu@gmail.com>
2023-09-04 12:30:54 +0000

    Playing with more array functions

--- arrays.php
+++ arrays.php
@@ -19,3 +19,30 @@ print_r(array_values($even));
 
 $array = array_map(fn($nums, $even) => $nums * $even, $nums, $even);
 print_r($array);
+
+echo '<br/>';
+$diff =  array_diff($array_1, $even);
+print_r($diff);
+
+$combed = array_merge($array_1, $even);
+echo '<br/>';
+
+print_r($combed);
+
+
+echo '<h2>Playing with Search </h2>';
+print_r($even);
+echo '<br/>';
+
+if (in_array(7, $even)) {
+    echo array_search(2, $even);
+}else {
+    echo "Value not in array!";
+}
+
+echo '<h2>Playing with Destructors </h2>';
+
+// Destructuring an array
+[$a, $b, $c] = [1, 2, 4];
+echo $b;
+
--- practice.php
+++ practice.php
@@ -38,9 +38,74 @@ if (is_callable($f)) {
 // echo addAll(1, 2, 4, 5, 6, 7);
 $x = 2;
 
-$sum = function(...$nums) {
-    return array_sum($nums);
-};
+// $sum = function(...$nums) use ($x) {
+//     return $x + array_sum($nums);
+// };
 echo '<br/>';
 
-echo $sum(1, 2, 3, 4, 5, 6);
+// echo $sum(1, 2, 3, 4, 5, 6);
+
+$array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$doubledArr = array_map(function($element){
+    return $element * 2;
+}, $array1);
+
+print_r($doubledArr);
+
+
+function sum(Callable $callable, ...$nums): int | float {
+    return $callable(array_sum($nums));
+}
+
+function doubles(int $num) {
+    return $num * 2;
+}
+
+echo sum('doubles' , 1, 2, 3, 4, 5);
+
+$dobArr = array_map(fn($element) => $element * 2, $doubledArr);
+print_r($dobArr);
+
+echo '<br/>';
+// Time practice
+$currentTime = time();
+
+echo $currentTime + 6 * 24 * 60 * 60;
+echo '<br/>';
+
+echo $currentTime - 6 * 24 * 60 * 60;
+echo '<br/>';
+
+echo mktime(0, 0, 0, 9, 24);
+echo '<br/>';
+echo date('d F Y', mktime(0, 0, 0, 9, 24));
+
+echo date('d M Y', strtotime('yesterday'));
+
+
+
+// working with arrays
+$cars = ['toyota', 'lamborghini', 'nissan', 'fiat', 'mazda', 'ferrari', 'koenigsegg', 'aston martin', 'rimac', 'lexus', 'hyundai', 'tesla'];
+
+print_r( array_chunk($cars, 2));
+
+// creating or combining with array_combine
+$arr1 = ['name', 'age', 'school'];
+$arr2 = ['Prince Asiedu', 19, 'KNUST'];
+
+echo '<br/>';
+$combined = array_combine($arr1, $arr2);
+
+echo $combined['name'];
+
+
+$even = array_filter($array1, fn($element) => $element % 2 == 0);
+
+print_r(array_values($even));
+echo '<br/>';
+$arr1 = [1, 2, 3];
+$arr2 = [4, 5, 6];
+
+$somethingHappened = array_map(null, $arr1, $arr2);
+print_r($somethingHappened);
